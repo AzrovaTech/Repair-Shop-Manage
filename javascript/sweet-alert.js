fed734155186934254e17010b8be5ff4850993ca
AzrovaTech <azrovatech@gmail.com>
2025-10-19 19:40:16 +0330

    finishing users page - part 3 - all users with features delete - edit- add + user orders & made orders and categories main page. + sweet alert

---
+++ javascript/sweet-alert.js
@@ -0,0 +1,22 @@
+function confirmDelete() {
+    console.log("xxxx")
+  Swal.fire({
+    title: "آیا از حذف این آیتم مطمعن هستید ؟",
+    text: "این عمل بدون بازگشت است !",
+    icon: "warning",
+    showCancelButton: true,
+    confirmButtonColor: "#ff5e00",
+    cancelButtonColor: "#3085d6",
+    confirmButtonText: "بله, حذف کن",
+    cancelButtonText: "انصراف"
+  }).then((result) => {
+    if (result.isConfirmed) {
+        Swal.fire({
+            title: "حذف شد!",
+            text: "آیتم مورد نظر با موفقیت حذف شد",
+            icon: "success",
+            confirmButtonText: "باشه"
+        });
+    }
+  });
+}
